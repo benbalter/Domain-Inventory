dc62522c0bfdfbc6d4ed53d1ffb04df2db3b4a93
Benjamin J. Balter <ben@balter.com>
2011-06-15 10:00:06 -0600

    plugin header

---
+++ domain-inspector.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Domain Inventory
+Plugin URI: 
+Description: 
+Version: 0.1
+Author: Benjamin J. Balter
+Author URI: http://ben.balter.com
+License: GPL2
+*/
+
+class DomainInspector {
+
+}
+
+
+
+?>
