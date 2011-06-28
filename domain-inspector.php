a9d91fc604c7e262df17bf92a8b504abfc99d83e
Benjamin J. Balter <ben@balter.com>
2011-06-28 14:31:38 -0600

    it works!

--- domain-inspector.php
+++ domain-inspector.php
@@ -14,16 +14,61 @@ class DomainInventory {
 	static $instance;
 	static $inspector;
 	
+	public $cts = array(
+			'status'=>array( 
+				'singular' => 'Server Status', 
+				'plural' => 'Server Statuses',
+			),
+			'nonwww'=>array( 
+				'singular' => 'Non-www. Support', 
+				'plural' => 'Non-www. Support',
+			),
+			'ipv6'=>array( 
+				'singular' => 'IPv6 support', 
+				'plural' => 'IPv6',
+			),
+			'cdn'=>array( 
+				'singular' => 'CDN Provider', 
+				'plural' => 'CDN Providers',
+			),
+			'ga'=>array( 
+				'singular' => 'Google Apps', 
+				'plural' => 'Google Apps',
+			),
+			'cms'=>array( 
+				'singular' => 'CMS', 
+				'plural' => 'CMSs',
+			),
+			'server_software'=>array( 
+				'singular' => 'Server Software', 
+				'plural' => 'Servers Software',
+			),
+			'cloud'=>array( 
+				'singular' => 'Cloud Provider', 
+				'plural' => 'Cloud Providers',
+			),
+			'analytics'=>array( 
+				'singular' => 'Analytics Source', 
+				'plural' => 'Analytics Sources',
+			),
+			'scripts'=>array( 
+				'singular' => 'Script Library', 
+				'plural' => 'Scripts Libraries',
+			),
+		);
+	
 	function __construct() {
 		
 		self::$instance = $this;
 		
 		//grab site inspector
 		require_once( 'site-inspector/class-site-inspector.php' );
-		self::$inspector = new SiteInspector;
+		$this->inspector = new SiteInspector;
 
-		add_action( 'init', array( $this, 'register_cpt' ) );
-		add_action( 'init', array( $this, 'register_cts' ) );
+		add_action( 'init', array( &$this, 'register_cpt' ) );
+		add_action( 'init', array( &$this, 'register_cts' ) );
+		add_action( 'admin_init', array( &$this, 'check_get' ) );
+		add_action( 'admin_init', array( &$this, 'meta_cb' ) );
 
 	}
 
@@ -41,73 +86,21 @@ class DomainInventory {
     'not_found' =>  __('No domains found'),
     'not_found_in_trash' => __('No domains found in Trash'), 
     'parent_item_colon' => '',
-    'menu_name' => 'Domains'
-
+    'menu_name' => 'Domains',
   );
   $args = array(
     'labels' => $labels,
     'public' => true,
-    'publicly_queryable' => true,
-    'show_ui' => true, 
-    'show_in_menu' => true, 
-    'query_var' => true,
-    'rewrite' => true,
-    'capability_type' => 'page',
     'has_archive' => true, 
-    'hierarchical' => true,
-    'menu_position' => null,
-    'supports' => array('title', 'comments', 'custom-fields','page-attributes')
+    'supports' => array('title', 'comments', 'custom-fields',	),
   ); 
   register_post_type('domain',$args);
 	
 	}
 	
 	function register_cts() {
-	
-		$cts = array(
-			'status'=>array( 
-				'singluar' => 'Server Status', 
-				'plural' => 'Server Statuses',
-			),
-			'non-www'=>array( 
-				'singluar' => 'Non-www. Support', 
-				'plural' => 'Non-www. Support',
-			),
-			'ipv6'=>array( 
-				'singluar' => 'IPv6 support', 
-				'plural' => 'IPv6',
-			),
-			'cdn'=>array( 
-				'singluar' => 'CDN Provider', 
-				'plural' => 'CDN Providers',
-			),
-			'ga'=>array( 
-				'singluar' => 'Google Apps', 
-				'plural' => 'Google Apps',
-			),
-			'cms'=>array( 
-				'singluar' => 'CMS', 
-				'plural' => 'CMSs',
-			),
-			'server'=>array( 
-				'singluar' => 'Server Software', 
-				'plural' => 'Servers Software',
-			),
-			'cloud'=>array( 
-				'singluar' => 'Cloud Provider', 
-				'plural' => 'Cloud Providers',
-			),
-			'analytics'=>array( 
-				'singluar' => 'Analytics Source', 
-				'plural' => 'Analytics Sources',
-			),
-			'scripts'=>array( 
-				'singluar' => 'Script Library', 
-				'plural' => 'Scripts Libraries',
-			),
-		);
 		
-		foreach ( $cts as $ct=>$names ) {
+		foreach ( $this->cts as $ct=>$names ) {
 					  
 			 $labels = array(
 				'name' => _x( $names['singular'], 'taxonomy general name' ),
@@ -116,16 +109,83 @@ class DomainInventory {
 				'all_items' => __( 'All ' . $names['plural'] ),
 				'parent_item' => __( 'Parent ' . $names['singular'] ),
 				'parent_item_colon' => __( 'Parent ' . $names['singular']. ':' ),
-				'edit_item' => __( 'Edit ' . $names['singluar'] ), 
-				'update_item' => __( 'Update ' . $names['singluar'] ),
-				'add_new_item' => __( 'Add New ' . $names['singluar']),
-				'new_item_name' => __( 'New ' . $names['singluar'] .' Name' ),
-				'menu_name' => __( $names['singluar'] ),
+				'edit_item' => __( 'Edit ' . $names['singular'] ), 
+				'update_item' => __( 'Update ' . $names['singular'] ),
+				'add_new_item' => __( 'Add New ' . $names['singular']),
+				'new_item_name' => __( 'New ' . $names['singular'] .' Name' ),
+				'menu_name' => __( $names['singular'] ),
 			  ); 	
 			 
 			register_taxonomy( $ct, 'domain', array( 'labels' => $labels ) );
 		}
 	}
+	
+	function inspect( $post_id ) {
+
+		//get post
+		$post = get_post( $post_id );
+
+		//verify post
+		if ( !$post )
+			return false;
+
+		//get data
+		$data = $this->inspector->inspect( $post->post_title );
+	
+		//md5 body
+		add_post_meta( $post->ID, 'md5', $data['md5'], true );
+		
+		//ip
+		add_post_meta( $post->ID, 'ip', $data['ip'], true );
+		
+		//status, ipv6, non-www, cdn, and cloud checks
+		$cts = array( 'status', 'ipv6', 'nonwww', 'cdn', 'cloud', 'server_software', 'cms', 'analytics', 'scripts');
+				
+		foreach ( $cts as $ct ) {
+			
+			if ( !isset( $data[$ct] ) )
+				continue;
+				
+			wp_set_post_terms( $post->ID, $data[$ct], $ct, true);
+		
+		}
+		
+		//inspected flag
+		add_post_meta( $post->ID, 'inspected', true , true );
+		
+		//return data
+		return $data;
+		
+			}
+	
+	function inspect_the_uninspected() {
+		global $wpdb;
+		$domains = $wpdb->get_col( "SELECT id FROM wp_posts WHERE wp_posts.post_type = 'domain' AND id NOT IN (SELECT post_id from wp_postmeta WHERE meta_key = 'inspected')" );
+				
+		foreach ($domains as $domain) 
+			$this->inspect( $domain );
+	
+	}
+	
+	function check_get( ) {
+		if ( !isset( $_GET['domain-inspect'] ) )
+			return;
+
+		if ( $_GET['post'] ) 
+			$this->inspect( $_GET['post'] );
+		else
+			$this->inspect_the_uninspected();
+			
+	}
+	
+	function meta_cb() {
+		add_meta_box( 'refresh', 'Refresh', array(&$this, 'refresh_metabox'), 'domain' );
+	}
+	
+	function refresh_metabox( $post ) { ?>
+		<a href="<?php echo add_query_arg( 'domain-inspect', true ); ?>">Refresh Data</a>
+	<?php }
+
 	
 }
 
